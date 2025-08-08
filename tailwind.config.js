51de57c7d96f179913bc760d3b9d3a1a35c29847
deva3xd <devarn05@gmail.com>
2025-08-08 21:18:01 +0700

    add custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
-export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
+export const content = ["./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
   extend: {
     backgroundImage: {
       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    },
+    boxShadow: {
+      default: "6px 6px 0px 0px #000",
     },
   },
 };
