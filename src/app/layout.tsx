8ed79eae16ff1322e173063b83428b1ebe2d9b6b
deva3xd <devarn05@gmail.com>
2025-08-22 22:14:31 +0700

    update tab spaces

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,18 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "YourTodo",
-  description: "A simple todo app wiith brutalism design",
+    title: "YourTodo",
+    description: "A simple todo app wiith brutalism design",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
